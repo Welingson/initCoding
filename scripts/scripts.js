8e3202744d1a7a4137097d19c68700ec89c94954
Welingson Expedito dos Santos <welingson12@gmail.com>
2024-03-25 22:24:08 -0300

    feat: style

---
+++ scripts/scripts.js
@@ -0,0 +1,16 @@
+/**
+ * ReactJs: 2 anos de experiência com ReactJs juntamente com:
+ * Styled Components, Tailwind CSS, Shadcn-UI e TypeScript e testes
+ */
+
+/**
+ * NodeJs: 2 anos de experiência em NodeJS juntamente com: TypeScript
+ * Sequelize, Prisma ORM, Fastify e ExpressJs
+ */
+
+/**
+ * Rest API: 2 anos de experiência no desenvovimento de API´s usando NodeJS 
+ * com as melhores práticas, incluindo: testes E2E, conceitos SOLID e Design Patterns
+ */
+
+
